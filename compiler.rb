908ca15f2dd2283c814570b3119fd8f90bb9e0fb
Viktharien <viktharien@zoho.com>
2018-05-16 19:50:56 +0000

    create parser

--- compiler.rb
+++ compiler.rb
@@ -127,6 +127,7 @@ Token = Struct.new(:type, :value)
 DefNode = Struct.new(:name, :arg_names, :body)
 IntegerNode = Struct.new(:value)
 CallNode = Struct.new(:name, :arg_exprs)
+VarRefNode = Struct.new(:name)
 tokens = Tokenizer.new(File.read("test.src")).tokenize
 
 puts tokens.map(&:inspect).join("\n")
