e57df7227f9d309cb8d69dc22eaaf61e3104c6c5
Viktharien <viktharien@zoho.com>
2018-05-16 19:29:21 +0000

    create parser

--- compiler.rb
+++ compiler.rb
@@ -7,7 +7,7 @@ class Tokenizer
         [:integer, /\b[0-9]+\b/],
         [:oparen, /\(/],
         [:cparen, /\)/],
-        [:comma, /,/]
+        [:comma, /,/],
         ]
     def initialize(code)
        @code = code 
